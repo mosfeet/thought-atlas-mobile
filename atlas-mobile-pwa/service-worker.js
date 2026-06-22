0a47521a1faf81f088284c8ec8a95f0e9ef24113
mosfeet <73884645+mosfeet@users.noreply.github.com>
2026-06-22 14:28:54 +0900

    Add service worker

---
+++ atlas-mobile-pwa/service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "thought-atlas-mobile-v9";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css?v=9",
+  "./app.js?v=9",
+  "./manifest.json",
+  "./data/sample-atlas.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
